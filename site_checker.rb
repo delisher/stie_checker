d1a97f138e9cf37900a5cf72b36061289b81a442
Sergei Smirnov <smirnov@programpark.ru>
2016-12-03 23:50:22 +0300

    Добавил файл с настройками и его обработку, поправил сообщения
    Добавил settings.yml с настройками,
    Добавил вывод "Дальнейшие уведомления отключены" после последней проверки и сообщение о восстановлении работы.
    Вынес статус ресурса в переменную.
    Поправил обработку счетчика

--- site_checker.rb
+++ site_checker.rb
@@ -5,21 +5,33 @@ require 'yaml'
 
 class SiteChecker
   MINUTE = 60
+  SETTINGS_PATH = "./settings.yml"
   
   def initialize
     update_time
+    @state = true
     @last_alert_minutes = 0
-    minutes = [29,11,47,21,33]
-    extra_messages = {14 => "Extra message test!"}
+    minutes = [3,10,50,100,500]
+    extra_messages = {}
     @settings ||= {
       minutes:        (minutes|extra_messages.keys).sort,
-      extra_messages:  extra_messages,
-      pause:           MINUTE
+      extra_messages: extra_messages,
+      pause:           1
     }
+    @settings.merge!(get_settings_from_file)
+    puts @settings
   end
 
-  def check_site url_text
-    uri = URI.parse(url_text)
+  def get_settings_from_file
+    settings = YAML.load_file(SETTINGS_PATH)
+    settings[:minutes] = (settings[:minutes] | settings[:extra_messages].keys).sort
+    settings
+  rescue Exception => err
+    puts "Can't read settings file'#{SETTINGS_PATH}'!\n#{err.backtrace.join("\n")}"
+    {}
+  end
+
+  def check_site uri
     response = Net::HTTP.get_response(uri)
     response.is_a? Net::HTTPSuccess
   rescue Exception => err
@@ -27,34 +39,49 @@ class SiteChecker
   end
 
   def update_time
-    @available_last = Time.now
+    @available_last_time = Time.now
   end
 
   def minute_for_alert
-    unavailable_time = (Time.now - @available_last).to_i * MINUTE
+    unavailable_time = (Time.now - @available_last_time).to_i / MINUTE
     alert_minute = @settings[:minutes].find{|m| @last_alert_minutes < m && unavailable_time >= m}
-    puts unavailable_time
     @last_alert_minutes = alert_minute if alert_minute
   end
 
-  def send_message
-    msg = if minute_for_alert
-      @settings[:extra_messages][@last_alert_minutes] ||
-      "Сервис недоступен более #{@last_alert_minutes} минут#{'ы' if (@last_alert_minutes % 10 == 1) && (@last_alert_minutes % 100 != 11) }"
+  def message_text
+    if @state
+      "Работа ресурса #{@url_text} восстановлена"
+    elsif minute_for_alert
+      msg = @settings[:extra_messages][@last_alert_minutes]
+      unless msg
+        msg = "Ресурс #{@url_text} недоступен более #{@last_alert_minutes} минут#{'ы' if (@last_alert_minutes % 10 == 1) && (@last_alert_minutes % 100 != 11) }"
+        msg << ". Дальнейшие уведомления отключены." if @settings[:minutes].last == @last_alert_minutes
+      end
+      msg
     end
+  end
+
+  def send_message
+    msg = message_text
     puts msg if msg
   end
 
   def start_checking_loop url_text, pause
+    @url_text = url_text
+    uri = URI.parse(url_text)
     while true
-      if check_site(url_text)
+      if check_site(uri)
         update_time
+        unless @state
+          @state = true
+          send_message
+        end
         @last_alert_minutes = 0
-        puts "Все хорошо"
       else
+        @state = false if @state
         send_message
       end
-      sleep @settings[:pause]
+      sleep @settings[:pause] * MINUTE
     end
   end
 end
